f0fbeb279604472baa77bafdeac82b79a5a473bf
VictorLoren <VictorLoren@users.noreply.github.com>
2016-02-21 14:21:54 -0800

    Update catalog database to include 'Catalog' and 'Item ' tables

---
+++ vagrant/catalog/catalog.py
@@ -0,0 +1,31 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, Date, Numeric
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+# Category of items in the catalog
+class Category(Base):
+    __tablename__ = 'category'
+    id = Column(Integer, primary_key = True)
+    name =Column(String(250), nullable = False)
+
+# Items in the catalog
+class Item(Base):
+    __tablename__ = 'item'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(250), nullable = False)
+    # Limit description so it doesn't take up too much (Twitter's new limit)
+    description = Column(String(10000))
+    # The date will only include the day (not the hour:minute:second)
+    date_added = Column(Date)
+    image = Column(String)
+    # Can have multiple Category IDs for the same item
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+
+
+engine = create_engine('sqlite:///catalog.db')
+
+Base.metadata.create_all(engine)

42aceb1cb14bfc9615796a2131efd217bc9882e4
VictorLoren <VictorLoren@users.noreply.github.com>
2016-02-21 17:22:00 -0800

    Add webserver code

---
+++ vagrant/catalog/webserver.py
@@ -0,0 +1,28 @@
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+
+class webServerHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        output = ""
+        output += "<html><body>Hello!</body></html>"
+        self.wfile.write(output)
+        print output
+        return
+
+
+def main():
+    try:
+        port = 8080
+        server = HTTPServer(('', port), webServerHandler)
+        print "Web Server running on port %s" % port
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print " ^C entered, stopping web server...."
+        server.socket.close()
+
+if __name__ == '__main__':
+    main()

fe7ffa5a7cc707651c3a77fbe60165b0ee60def8
VictorLoren <VictorGeislinger+github@gmail.com>
2016-04-23 14:34:57 -0700

    refactor: Change server code to use Flask framework

--- vagrant/catalog/webserver.py
+++ vagrant/catalog/webserver.py
@@ -1,72 +1,48 @@
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-import cgi
+from flask import Flask
+app = Flask(__name__)
 
-# import CRUD Operations from Lesson 1
-from catalog import Base, Category, Item
+# Import CRUD Operations from Lesson 1
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from catalog import Base, Category, Item
 
 # Create session and connect to DB
 engine = create_engine('sqlite:///catalog.db')
 Base.metadata.bind = engine
+
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+# CSS
+@app.route('/styles.css')
+def style():
+    with open('website_template/styles.css','r') as stylesheet:
+        output = stylesheet.read()
+    return output
 
-class webServerHandler(BaseHTTPRequestHandler):
-
-    def do_GET(self):
-        try:
-            # Allow for the stylesheet
-            if self.path.endswith('styles.css'):
-                categories = session.query(Category).all()
-                with open('website_template/styles.css','r') as stylesheet:
-                    output = stylesheet.read()
-                self.send_response(200)
-                self.send_header('Content-type', 'text/css')
-                self.end_headers()
-                self.wfile.write(output)
-                return
-            # Home page
-            elif self.path.endswith('/') or self.path.endswith('catalog/'):
-                categories = session.query(Category).all()
-                self.send_response(200)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-                # Find categories
-                output_categories = []
-                for category in categories:
-                    category_path = self.path + category.name
-                    output_categories.append('<a href="%s"><h3>%s</h3></a>'
-                                             % (category_path, category.name) )
-                all_categories = ''.join(output_categories)
-                # Find recent items
-                output_recent_items = []
-                all_recent_items = ''.join(output_recent_items)
-                # TODO(VictorLoren): Read in recent items (make it a list)
-                # Read in HTML template and replace parts
-                with open('website_template/index.html','r') as html_file:
-                    output = html_file.read()\
-                             .replace('%CATEGORIES', all_categories)\
-                             .replace('%RECENT_ITEMS', all_recent_items)
-                self.wfile.write(output)
-                return
-            # TODO(VictorLoren): Crete category page
-            # Wrong address
-            else:
-                self.send_error(404, 'File Not Found: %s' % self.path)
-        except IOError:
-            self.send_error(404, 'File Not Found: %s' % self.path)
-
-
-def main():
-    try:
-        server = HTTPServer(('', 8000), webServerHandler)
-        print 'Web server running...open localhost:8000/ in your browser'
-        server.serve_forever()
-    except KeyboardInterrupt:
-        print '^C received, shutting down server'
-        server.socket.close()
+# Home page
+@app.route('/')
+@app.route('/catalog')
+def catalog():
+    categories = session.query(Category).all()
+    # Find categories
+    output_categories = []
+    for category in categories:
+        category_path = category.name
+        output_categories.append('<a href="catalog/%s"><h3>%s</h3></a>'
+                                 % (category_path, category.name) )
+    all_categories = ''.join(output_categories)
+    # Find recent items
+    output_recent_items = []
+    all_recent_items = ''.join(output_recent_items)
+    # TODO(VictorLoren): Read in recent items (make it a list)
+    # Read in HTML template and replace parts
+    with open('website_template/index.html','r') as html_file:
+        output = html_file.read()\
+                 .replace('%CATEGORIES', all_categories)\
+                 .replace('%RECENT_ITEMS', all_recent_items)
+    return output
 
 if __name__ == '__main__':
-    main()
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)

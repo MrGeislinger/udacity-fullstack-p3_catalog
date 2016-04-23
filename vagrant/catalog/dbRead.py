0b960421c55004657136bdf277365c409539793d
VictorLoren <VictorGeislinger+github@gmail.com>
2016-04-23 13:18:19 -0700

    style: Change double-quotes to single-quotes

--- vagrant/catalog/dbRead.py
+++ vagrant/catalog/dbRead.py
@@ -14,7 +14,7 @@ session = DBSession()
 
 def readItems():
     for c in session.query(Item):
-      print "%s #%d (Added %s)" %(c.name, c.id, c.date_added)
-      print "Category: #%d - %s" %(c.category_id,session.query(Category).filter_by(id=c.category_id).first().name)
-      print "Description: \"\n\t%s\"" %c.description
-      print "Image: %s\n" %c.image
+      print '%s #%d (Added %s)' %(c.name, c.id, c.date_added)
+      print 'Category: #%d - %s' %(c.category_id,session.query(Category).filter_by(id=c.category_id).first().name)
+      print 'Description: \'\n\t%s\'' %c.description
+      print 'Image: %s\n' %c.image

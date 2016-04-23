25a3601d0a5f713e1c2315c160e9924c717b7623
VictorLoren <VictorGeislinger+github@gmail.com>
2016-04-23 15:46:58 -0700

    refactor: Use Flask's templates for HTML pages

--- vagrant/catalog/webserver.py
+++ vagrant/catalog/webserver.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 # Import CRUD Operations from Lesson 1
@@ -24,55 +24,26 @@ def style():
 @app.route('/')
 @app.route('/catalog')
 def catalog():
-    categories = session.query(Category).all()
     # Find categories
-    output_categories = []
-    for category in categories:
-        category_path = category.name
-        output_categories.append('<a href="catalog/%s"><h3>%s</h3></a>'
-                                 % (category_path, category.name) )
-    all_categories = ''.join(output_categories)
-    # Find recent items
-    output_recent_items = []
-    all_recent_items = ''.join(output_recent_items)
+    categories = session.query(Category).all()
     # TODO(VictorLoren): Read in recent items (make it a list)
-    # Read in HTML template and replace parts
-    with open('website_template/index.html','r') as html_file:
-        output = html_file.read()\
-                 .replace('%CATEGORIES', all_categories)\
-                 .replace('%RECENT_ITEMS', all_recent_items)
-    return output
+    recent_items = []
+    return render_template('index.html', categories=categories,
+                            recent_items=recent_items)
 
 # Category page
 @app.route('/catalog/<string:category_name>/')
 def category(category_name):
     # Find categories
     categories = session.query(Category).all()
-    output_categories = []
-    for category in categories:
-        category_path = category.name
-        # Go back to "HOME/catalog/CATEGORY"
-        output_categories.append('<a href="../%s"><h3>%s</h3></a>'
-                                 % (category_path, category.name) )
-    all_categories = ''.join(output_categories)
-    # Get items from category
+    # TODO(VictorLoren): Read in recent items (make it a list)
+    recent_items = []
+    #Get items from category
     cat_id = session.query(Category).filter(Category.name == category_name)\
              .first().id
     items = session.query(Item).filter(Item.category_id == cat_id)
-    output_items = []
-    for item in items:
-        item_path = item.name
-        output_items.append('<li><a href="%s">%s</a></li>'
-                                 % (item_path, item.name) )
-    all_items = ''.join(output_items)
-    print all_items
-    # Read in HTML template and replace parts
-    with open('website_template/category.html','r') as html_file:
-        output = html_file.read()\
-                 .replace('%CATEGORIES', all_categories)\
-                 .replace('%CATEGORY', category_name)\
-                 .replace('%ITEMS', all_items)
-    return output
+    return render_template('category.html', categories=categories, items=items,
+                            recent_items=recent_items, category=category)
 
 if __name__ == '__main__':
     app.debug = True
